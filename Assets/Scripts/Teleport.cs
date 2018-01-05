396d3d3d844c6e8fcabd43ab12783e65e201e87f
Benjamin milian <vinyelortiz@gmail.com>
2018-01-05 11:32:34 -0500

    First commit into main

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vodgets
+{
+    public class Teleport : Vodget
+    {
+        public SteamVR_TrackedController controller_left;
+        public SteamVR_TrackedController controller_right;
+        public Transform VRperson_pos = null;
+        public LineRenderer lineray;
+        
+  
+
+        // Use this for initialization
+        void Start()
+        {
+            lineray.enabled = false;
+          
+        }
+
+        public override void DoFocus(Selector cursor, bool state)
+        {
+            base.DoFocus(cursor, state);
+            Debug.Log("DoFocus" + cursor.Cursor.localPosition.ToString() );
+        }
+
+        public override void DoGrab(Selector cursor, bool state)
+        {
+            base.DoGrab(cursor, state);
+            Debug.Log("DoGrab" + cursor.Cursor.localPosition.ToString());
+            //if (state)
+            //    VRperson_pos.position = cursor.Cursor.localPosition;
+           
+        }
+
+        public override void DoButton(Selector cursor, Selector.Button button, bool state)
+        {
+            Debug.Log("Button:" + button + " State:" + state);
+            
+            if (state && button == Selector.Button.PadClicked)
+            {
+                lineray.enabled = true;
+                SteamVR_Fade.Start(Color.clear, 0f);
+                SteamVR_Fade.Start(Color.black, 1f);
+                VRperson_pos.position = cursor.Cursor.localPosition;
+                SteamVR_Fade.Start(Color.black, 0f);
+                SteamVR_Fade.Start(Color.clear, 1f);
+            }
+                
+        }
+
+        public override void DoUpdate(Selector cursor)
+        {
+            base.DoUpdate(cursor);
+            //Debug.Log("DoUpdate" + cursor.Cursor.localPosition.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/sound_interaction.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vodgets
+{ 
+    public class sound_interaction : Vodget
+    {
+        AudioSource sound;
+        bool touch_active = false;
+
+        public BoolEvent onPlaySound = new BoolEvent();
+
+        public void PlaySound( bool state )
+        {
+            if (state)
+            {
+                sound.Play();
+            }
+        }
+
+        void Start()
+        {
+            sound = GetComponent<AudioSource>();
+            RegisterState(onPlaySound, PlaySound);
+        }
+        
+        public override void DoFocus(Selector cursor, bool state)
+        {
+            base.DoFocus(cursor, state);
+            onPlaySound.Invoke(state);
+            //PlaySound(state);
+        }
+
+        //public override void DoGrab(Selector cursor, bool state)
+        //{
+        //    base.DoGrab(cursor, state);
+        //}
+
+        //public override void DoButton(Selector cursor, Selector.Button button, bool state)
+        //{
+        
+        //}
+
+        //public override void DoUpdate(Selector cursor)
+        //{
+        //    base.DoUpdate(cursor);
+           
+        //}
+
+    }
+}
